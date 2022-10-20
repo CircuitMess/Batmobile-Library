1485bc1d5227d2e90402a70ecc5d97f2f06acdb4
Filip Budiša <filip@circuitmess.com>
2022-10-20 21:26:22 +0200

    Add correct pin definitions

--- src/Pins.hpp
+++ src/Pins.hpp
@@ -1,39 +1,25 @@
 #ifndef BATMOBILE_LIBRARY_PINS_H
 #define BATMOBILE_LIBRARY_PINS_H
 
-#define I2C_SDA 14
-#define I2C_SCL 15
+#define MOTOR_FL_A 13
+#define MOTOR_FL_B 2
+#define MOTOR_FR_A 27
+#define MOTOR_FR_B 14
+#define MOTOR_BL_A 19
+#define MOTOR_BL_B 18
+#define MOTOR_BR_A 33
+#define MOTOR_BR_B 26
 
-#define BTN_UP 3
-#define BTN_DOWN 1
-#define BTN_LEFT 0
-#define BTN_RIGHT 2
-#define BTN_MID 4
-#define BTN_BACK 5
+#define LED_R 23
+#define LED_G 21
+#define LED_B 22
 
-#define MOTOR_FL 0
-#define MOTOR_BL 1
-#define MOTOR_FR 2
-#define MOTOR_BR 3
+#define LED_FRONT 12
+#define LED_BACK 32
 
-// camera pins
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      4
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#define PIN_SPKR 25
 
-#define PIN_JIG 35
+#define PIN_BATT 39
+#define PIN_CHARGE -1
 
 #endif //BATMOBILE_LIBRARY_PINS_H

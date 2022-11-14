c91a12aa85a56c0e569a637265cad2d344d0dc9e
subo <gamedevsubo@gmail.com>
2022-11-14 14:04:26 +0100

    Add SignalSender class

---
+++ src/Communication/SignalSender.cpp
@@ -0,0 +1,38 @@
+#include <Loop/LoopManager.h>
+#include <esp_wifi.h>
+#include "SignalSender.h"
+#include "Communication.h"
+#include "DecibelPercentConverter.h"
+
+SignalSender::SignalSender(){}
+
+SignalSender::~SignalSender(){
+	LoopManager::removeListener(this);
+}
+
+void SignalSender::begin(){
+	LoopManager::addListener(this);
+}
+
+void SignalSender::loop(uint micros){
+	microCounter += micros;
+	if(microCounter >= interval){
+		microCounter = 0;
+		Com.sendSignalStrength(getStrength());
+	}
+}
+
+uint8_t SignalSender::getStrength(){
+	wifi_ap_record_t info;
+	uint8_t percentage = 0;
+
+	if(!esp_wifi_sta_get_ap_info(&info)) {
+		for (int x = 0; x < 100; x = x + 1){
+			if(signalDecibel[x] == info.rssi){
+				percentage = signalPercent[x];
+				break;
+			}
+		}
+	}
+	return percentage;
+}
---
+++ src/Communication/SignalSender.h
@@ -0,0 +1,25 @@
+#ifndef BATMOBILE_LIBRARY_SIGNALSENDER_H
+#define BATMOBILE_LIBRARY_SIGNALSENDER_H
+
+
+#include <Loop/LoopListener.h>
+#include <esp_wifi_types.h>
+
+class SignalSender : private LoopListener {
+
+public:
+	SignalSender();
+	~SignalSender() override;
+
+	void begin();
+
+private:
+	void loop(uint micros) override;
+	uint8_t getStrength();
+
+	uint32_t microCounter = 0;
+	static constexpr uint32_t interval = 1000000;
+};
+
+
+#endif //BATMOBILE_LIBRARY_SIGNALSENDER_H

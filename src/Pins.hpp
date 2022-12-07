d811701782c76f4efbb29a7b216f92222b6911aa
Filip Budiša <filip@circuitmess.com>
2022-12-07 11:02:09 +0100

    Update pins

--- src/Pins.hpp
+++ src/Pins.hpp
@@ -1,22 +1,25 @@
 #ifndef BATMOBILE_LIBRARY_PINS_H
 #define BATMOBILE_LIBRARY_PINS_H
 
-#define MOTOR_FL_A 13
-#define MOTOR_FL_B 2
-#define MOTOR_FR_A 27
-#define MOTOR_FR_B 14
-#define MOTOR_BL_A 19
-#define MOTOR_BL_B 18
+#define MOTOR_FL_A 21
+#define MOTOR_FL_B 22
+#define MOTOR_FR_A 23
+#define MOTOR_FR_B 25
+#define MOTOR_BL_A 18
+#define MOTOR_BL_B 19
 #define MOTOR_BR_A 33
-#define MOTOR_BR_B 26
+#define MOTOR_BR_B 32
 
-#define LED_R 23
-#define LED_G 21
-#define LED_B 22
+#define S3_MOSI 17
+#define S3_MISO 16
+#define S3_CLK 5
+#define S3_CS 4
+#define S3_RST 13
+#define S3_INT 34
 
-#define LED_R 15
-#define LED_G 13
-#define LED_B 12
+#define LED_R 12
+#define LED_G 15
+#define LED_B 2
 
 #define LED_FRONT 14
 #define LED_BACK 27

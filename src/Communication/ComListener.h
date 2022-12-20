16e50faeeb219434e069469b5646b10869296409
Emil Gajšak <emil.gajsak@gmail.com>
2022-12-20 16:59:42 +0100

    Added onShutdown to ComListener, sending shutdownAck

--- src/Communication/ComListener.h
+++ src/Communication/ComListener.h
@@ -9,23 +9,25 @@ public:
 	/**
 	 * @param hue HSV format hue divided by 2 [0-180]
 	 */
-	virtual void onBallHue(uint8_t hue);
-	virtual void onBoost(bool boost);
+	virtual void onBallHue(uint8_t hue){};
+	virtual void onBoost(bool boost){};
 	/**
 	 * @param dir Bitwise OR-ed values of direction buttons,
 	 * lowest to highest bit represents forward, backward, left, right, respectively
 	 */
-	virtual void onDriveDir(uint8_t dir);
-	virtual void onDriveMode(DriveMode mode);
+	virtual void onDriveDir(uint8_t dir){};
+	virtual void onDriveMode(DriveMode mode){};
 	/**
 	 * @param speed [0-100]
 	 */
-	virtual void onDriveSpeed(uint8_t speed);
-	virtual void onHonk();
+	virtual void onDriveSpeed(uint8_t speed){};
+	virtual void onHonk(){};
 	/**
 	 * @param volume [0-100]
 	 */
-	virtual void onVolume(uint8_t volume);
+	virtual void onVolume(uint8_t volume){};
+
+	virtual void onShutdown(){};
 };
 
 
--- src/Communication/Communication.h
+++ src/Communication/Communication.h
@@ -18,10 +18,10 @@ public:
 
 	void sendBattery(uint8_t percent, bool charging);
 	void sendSignalStrength(uint8_t percent);
-	void sendShutdown(std::function<void()> ackCallback);
+	void sendShutdownAck();
 
 	void addListener(ComType type, ComListener* listener);
-    void addListener(std::initializer_list<ComType> type,ComListener* listener);
+	void addListener(std::initializer_list<ComType> type, ComListener* listener);
 	void removeListener(ComType type, ComListener* listener);
 	void removeListener(ComListener* listener);
 

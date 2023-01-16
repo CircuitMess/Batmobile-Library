3686892f4ed6d03ddb8e3b47061e4153fa18ce18
subo <gamedevsubo@gmail.com>
2023-01-16 15:47:59 +0100

    Add shutdown() to BatmobileImpl

--- src/Batmobile.cpp
+++ src/Batmobile.cpp
@@ -39,3 +39,28 @@ void BatmobileImpl::begin(){
 	}
 }
 
+void BatmobileImpl::shutdown() {
+    Com.end();
+    adc_power_off();
+    WiFi.disconnect(true);
+    WiFi.mode(WIFI_OFF);
+    btStop();
+
+    Underlights.clear();
+    Taillights.clear();
+    Headlights.clear();
+    Motors.stopAll();
+    Audio.stop();
+    //TODO - power down S3 as well
+
+    const uint8_t deinitPins[] = { LED_FRONT, LED_BACK, LED_R, LED_G, LED_B, MOTOR_FR_A, MOTOR_FL_A, MOTOR_BR_A, MOTOR_BL_A };
+    for(const auto& pin: deinitPins){
+        ledcDetachPin(pin);
+    }
+
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+    esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
+    esp_deep_sleep_start();
+}
